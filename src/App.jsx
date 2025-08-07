635ca8cb15b74c28d422a0bfdcfe83c99c917d00
Minh Nguyen <tuanminh10082002gv@gmail.com>
2025-08-07 19:44:43 +0200

    gameboard

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,23 @@
 
 function App() {
+  return (
+    <main>
+      <div id='game-container'>
+        <ol id ='player'>
+          <li>
+            <span className="player-name"> Player 1</span>
+            <span className="player-symbol">X</span>
+          </li>
+          <li>
+            <span className="player-name"> Player 2</span>
+            <span className="player-symbol">O</span>
+          </li>
+        </ol>
+        GAME BOARD
+      </div>
+
+    </main>
+    );
 }
 
 export default App
