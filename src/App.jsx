5433dcf0feaf15fd76aa099d424fbd92dc8ec84d
Minh Nguyen <tuanminh10082002gv@gmail.com>
2025-08-17 13:05:40 +0200

    refactor removed player state

--- src/App.jsx
+++ src/App.jsx
@@ -3,18 +3,23 @@ import GameBoard from "./components/GameBoard.jsx";
 import Log from "./components/Log.jsx";
 import { useState } from "react";
 
+function deriveActivePlayer(gameTurns) {
+  let currentPlayer = 'X'
+
+  if (gameTurns.length > 0 && gameTurns[0].player === 'X') {
+      currentPlayer = 'O'
+    }
+
+  return currentPlayer;
+}
+
 function App() {
   const [gameTurns, setGameTurns] = useState([]);
-  const [currentPlayer, setCurrentPlayer] = useState("X");
+  const currentPlayer = deriveActivePlayer(gameTurns);
 
   function handleCellClick(rowIndex, colIndex) {
-    setCurrentPlayer((prevPlayer) => (prevPlayer === "X" ? "O" : "X"));
     setGameTurns((prevTurns) => {
-      let currentPlayer = 'X'
-
-      if (prevTurns.length > 0 &&prevTurns[0].player === 'X') {
-        currentPlayer = 'O'
-      }
+      const currentPlayer = deriveActivePlayer(prevTurns);
 
       const updatedTurns = [
         { square: { row: rowIndex, col: colIndex }, player: currentPlayer }, 
